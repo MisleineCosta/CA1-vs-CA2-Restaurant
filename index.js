82bffae79831d4cee53f9d9feed76d60a96177fd
MisleineCosta <costaleine@icloud.com>
2021-02-26 15:53:42 +0000

    new chances

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res)=>{
-  res.write("My first API - MC \n"); // write a response
+  res.write("My first API  MC \n"); // write a response
   res.end(); //end the response
 }).listen(8000); // listen for requests on port 8000
 
