37a909b23be66ddccfa3b534c8fd773b8fe3e443
MisleineCosta <costaleine@icloud.com>
2021-02-26 18:00:40 +0000

    chances

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const http = require('http');
 http.createServer((req, res)=>{
   res.write("My first API - MC \n"); // write a response
   res.end(); //end the response
-}).listen(8000); // listen for requests on port 8000
+}).listen(3000); // listen for requests on port 3000
+
+ let users = []; // names of users will be stored here
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(({ data }) => {
+      users = data.map(user => user.name); // get only the names of the users and store in an array
+    })
+    .catch(error=>{
+      console.log(error);
+    });
 
