f11765eae1b66955b7cfbce9020efdf80092142d
MisleineCosta <costaleine@icloud.com>
2021-03-09 11:23:40 +0000

    changes wk4

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-const http = require('http');
+const http = require('http'),
+axios = require('axios'),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'),
+bodyParser = require('body-parser');
 
-http.createServer((req, res)=>{
-  res.write("My first API - MC \n"); // write a response
-  res.end(); //end the response
-}).listen(3000); // listen for requests on port 3000
+var app = express();
+var port = 3000;
 
- let users = []; // names of users will be stored here
-    axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(({ data }) => {
-      users = data.map(user => user.name); // get only the names of the users and store in an array
-    })
-    .catch(error=>{
-      console.log(error);
-    });
+app.use(bodyParser.json())
 
+app.get('/ola/:fog/:bar', (req, res) => {
+    res.json({
+        message: 'Greeting all! ', data: [  
+        req.params.fog,
+        req.params.bar
+        ]});
+    //res.json({ message: 'Greeting all!\n\n' + users.join('\n')});
+});
+
+ //second external API
+//overpoind https://swapi.dev/api/people
+
+// http.createServer((req, res) => {
+//     res.write("Hello Mikhail :) \n..........................\n\n*\n"); // in order to write a response
+//     res.write(users.join("\n \n *\n")); // display the list of users on 
+//     res.write("\n\n\n............................\nE-MAILS:\n............................\n\n\n");
+//     res.write(emails.join("\n\n __________________________\n"));
+//     res.end(); //over the response
+
+//}).listen(3000); // for requesting on port 8000  
+
+let users = ["..."]; // names of users will be stored here
+let emails = ["..."]; // outgoing email list
+(async function getNames() {
+
+    //second external API
+    //overpoind https://swapi.dev/api/people
+
+    try {
+        const { data } = await axios.get("https://swapi.dev/api/films");//("https://swapi.dev/api/names/users");
+        users = data.map(user => user.name);
+        emails = data.map(email => email.email);
+        console.log(users);
+        console.log(emails);
+        
+    } catch (error) {
+        console.log(error)
+    }
+})();
+
+app.listen(port, function (err) {
+    console.log('Listening on port: ' + port);
+});
