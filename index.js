d2382cd29175325f3022c4869f059996af5b9b63
MisleineCosta <costaleine@icloud.com>
2021-03-09 12:21:12 +0000

    add

--- index.js
+++ index.js
@@ -10,27 +10,22 @@ var port = 3000;
 
 app.use(bodyParser.json())
 
-app.get('/ola/:fog/:bar', (req, res) => {
-    res.json({
-        message: 'Greeting all! ', data: [  
-        req.params.fog,
+app.get('/ola/:foo/:bar', (req, res) => {
+    res.json({message: 'Greeting all! ', data: [  
+        req.params.foo,
         req.params.bar,
         ]});
     //res.json({ message: 'Greeting all!\n\n' + users.join('\n')});
 });
 
- //second external API
-//overpoind https://swapi.dev/api/people
+http.createServer((req, res) => {
+    res.write("Hello Mikhail :) \n..........................\n\n*\n"); // in order to write a response
+    res.write(users.join("\n \n *\n")); // display the list of users on 
+    res.write("\n\n\n............................\nE-MAILS:\n............................\n\n\n");
+    res.write(emails.join("\n\n __________________________\n"));
+    res.end(); //over the response
 
-// http.createServer((req, res) => {
-//     res.write("Hello Mikhail :) \n..........................\n\n*\n"); // in order to write a response
-//     res.write(users.join("\n \n *\n")); // display the list of users on 
-//     res.write("\n\n\n............................\nE-MAILS:\n............................\n\n\n");
-//     res.write(emails.join("\n\n __________________________\n"));
-//     res.end(); //over the response
-
-//}).listen(3000); // for requesting on port 8000  
-
+}).listen(3000); // for requesting on port 8000  
 let users = ["..."]; // names of users will be stored here
 let emails = ["..."]; // outgoing email list
 (async function getNames() {
