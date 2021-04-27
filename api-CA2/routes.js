00260511ef39395a53a86fff4153f19694a63a4f
MisleineCosta <costaleine@icloud.com>
2021-04-27 16:14:14 +0000

    crud-routes.js

--- api-CA2/routes.js
+++ api-CA2/routes.js
@@ -4,19 +4,19 @@ router = express.Router();
 
 //reference into routes from the request logic intem-controller
 
-// //Book routes CRUD
-// bookCtrl = require('./book-controller');
+//Book routes CRUD
+bookCtrl = require('./book-controller');
 
-// router.post('/books', bookCtrl.createBook);//CREATE USER/ books are the table find on the collection
-// router.get('/books', bookCtrl.getBooks);//READ all
-// router.get('/books/:id', bookCtrl.getBook);//FIND USER BY id
-// router.put('/books/:id', bookCtrl.updateBook);//UPDATES USER BY id
-// router.delete('/books/:id', bookCtrl.deleteBook);//DELETE USER BY id
+router.post('/books', bookCtrl.createBook);//CREATE USER/ books are the table find on the collection
+router.get('/books', bookCtrl.getBooks);//READ all
+router.get('/books/:id', bookCtrl.getBook);//FIND USER BY id
+router.put('/books/:id', bookCtrl.updateBook);//UPDATES USER BY id
+router.delete('/books/:id', bookCtrl.deleteBook);//DELETE USER BY id
 
 //PRODUCT routes CRUD
 productCtrl = require('./product-controller');
 
-router.post('/product', productCtrl.createProduct);//CREATE USER/ books are the table find on the collection
+router.post('/products', productCtrl.createProduct);//CREATE USER/ books are the table find on the collection
 router.get('/products', productCtrl.getProducts);//READ all
 // router.get('/products/:id', productCtrl.getProduct);//FIND USER BY id
 // router.put('/products/:id', productCtrl.updateProduct);//UPDATES USER BY id
