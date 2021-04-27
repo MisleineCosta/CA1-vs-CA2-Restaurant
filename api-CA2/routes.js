abda8eaebff3c06684a6244f63e2a481b789a22c
MisleineCosta <costaleine@icloud.com>
2021-04-27 14:54:37 +0000

    product-routes-crud

--- api-CA2/routes.js
+++ api-CA2/routes.js
@@ -13,6 +13,17 @@ router.get('/books/:id', bookCtrl.getBook);//FIND USER BY id
 router.put('/books/:id', bookCtrl.updateBook);//UPDATES USER BY id
 router.delete('/books/:id', bookCtrl.deleteBook);//DELETE USER BY id
 
+//PRODUCT routes CRUD
+// bookCtrl = require('./book-controller');
+
+// router.post('/books', bookCtrl.createBook);//CREATE USER/ books are the table find on the collection
+// router.get('/books', bookCtrl.getBooks);//READ all
+// router.get('/books/:id', bookCtrl.getBook);//FIND USER BY id
+// router.put('/books/:id', bookCtrl.updateBook);//UPDATES USER BY id
+// router.delete('/books/:id', bookCtrl.deleteBook);//DELETE USER BY id
+
+
+
 
 //EXPORTS
 module.exports = router;
