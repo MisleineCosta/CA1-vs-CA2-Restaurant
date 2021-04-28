353e2de49f389939ca23cae7c523c7f3d963bd68
MisleineCosta <costaleine@icloud.com>
2021-04-28 03:04:23 +0000

    CRUD-update

---
+++ IWA-API-CA2/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const app = require('../node-restful-api-tutorial/app');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
+
+/*app.post("/users", userCtrl.createUser);
+app.get("/users", userCtrl.getUsers);
+app.get("/users/:id", userCtrl.getUser);
+app.delete("/users/:id", userCtrl.deleteUser);
+app.put("/users/:id", userCtrl.updateUser);
+// We need to define a variable for Express:
+var express = require("express"),
+    router = express.Router();
+app.use(require("./routes"));
+var express = require("express"),
+    router = express.Router(),
+    mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/costa");
+mongoose.connection.on("error", (err) => {
+    console.log(" Mongo database Error: ", err);
+    process.exit();
+});
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB is successfully connected");
+});
+*/
